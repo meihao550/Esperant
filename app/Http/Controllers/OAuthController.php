382a0f5f665fceebe41c0b9e6f7e0e21b0313d85
meihao550 <ziyin550@gmail.com>
2026-06-08 20:01:27 +0900

    update

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -34,7 +34,7 @@ class OAuthController extends Controller
             return redirect('/')->with('success', 'GitHubログインに成功しました！');
 
         } catch (Exception $e) {
-            return redirect('/auth/redirect/')->with('error', '認証に失敗しました: ' . $e->getMessage());
+            return redirect('/')->with('error', '認証に失敗しました: ' . $e->getMessage());
         }
     }
 }
